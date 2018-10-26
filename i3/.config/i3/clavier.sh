eef0e0f04b9f411815b714f7af68e5ce5bf20725
fabburst <40134787+fabburst@users.noreply.github.com>
2018-10-26 07:16:48 +0200

    Update clavier.sh

--- i3/.config/i3/clavier.sh
+++ i3/.config/i3/clavier.sh
@@ -4,6 +4,6 @@ echo "Changing keys..."
 xmodmap -e 'keycode 51 = less greater bar brokenbar bar brokenbar'
 xmodmap -e 'keycode 35 = dollar sterling bracketright braceright asterisk'
 
-echo "Changed keys!"
+echo "The keys have been changed !"
 echo
 
